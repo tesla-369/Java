849d9961bae6aa0341aa6711d2d459f2aefaae22
navraj neupane <60267553+tesla-369@users.noreply.github.com>
2023-01-03 04:34:06 +0545

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,15 @@
+//Compute area of a circle
+class Area 
+{
+	
+	public static void main(String args[])
+	{
+		double a,pi,r;
+		pi = 3.147;
+		r = 10.8;
+		a = pi * r * r;
+		System.out.print("Area of a circle is:" + a);
+		
+	}
+
+}
---
+++ CharDemo2.java
@@ -0,0 +1,12 @@
+//char variables behave like integers
+ class CharDemo2 {
+	 public static void main(String args[])
+	 {
+		char ch1;
+		ch1 = 'X';
+		System.out.println("Ch1 contains " + ch1);
+		ch1++;//increments ch1
+		System.out.println("the value of ch1 now is: "+ ch1);
+	 }
+
+}
