11b7161f83d4dd46d766dcfb928733575446fdbb
navraj neupane <60267553+tesla-369@users.noreply.github.com>
2023-01-03 03:48:07 +0545

    Add files via upload

---
+++ Example1.java
@@ -0,0 +1,15 @@
+
+/*Here is simple example
+ * call this "Example1.java"*/
+class Example1 {
+	
+	public static void main(String args[]) {
+		int num; // This declares variable called number//
+		num=100; // This assigns num the value 100 //
+		System.out.println("This is num:" + 100);
+		num = num * 2;
+		System.out.print("The vale of num * 2 is:");
+		System.out.println(num);
+		
+	}
+}
