5ec84989ab741fec4601148185b60df263d242e5
navraj neupane <60267553+tesla-369@users.noreply.github.com>
2023-01-04 03:49:36 +0545

    Add files via upload

---
+++ DoubleLongFloat.java
@@ -0,0 +1,19 @@
+
+public class DoubleLongFloat {
+
+	public static void main(String[] args) {
+		
+		int a = -6;
+		long b = 154444440L;
+		float c = 2.5f;
+		double d = 234567888888888.99;
+		
+		System.out.print(b);
+		System.out.println(a);
+		
+		
+		
+
+	}
+
+}
