85e1803aed59ead635d284e6d39c1570c9fc0265
navraj neupane <60267553+tesla-369@users.noreply.github.com>
2023-01-12 20:15:22 +0545

    Add files via upload

---
+++ JavaNotes/for_loop.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class for_loop {
+	
+	public static void main(String args[]) {
+		
+		int num;
+		System.out.println("Enter any Number: ");
+		Scanner s = new Scanner(System.in);
+		num = s.nextInt();
+		
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(num*i);
+		}
+	}
+
+}
