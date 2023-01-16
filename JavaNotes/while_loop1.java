fd583fa406166a8cf29c7d5227e73f81d5e0b392
navraj neupane <60267553+tesla-369@users.noreply.github.com>
2023-01-16 18:43:12 +0545

    Add files via upload

---
+++ JavaNotes/while_loop1.java
@@ -0,0 +1,17 @@
+// Java program to illustrate while loop.
+ 
+class while_loop1 {
+    public static void main(String args[])
+    {
+        // initialization expression
+        int i = 1;
+ 
+        // test expression
+        while (i < 6) {
+            System.out.println("Hello World");
+ 
+            // update expression
+            i++;
+        }
+    }
+}
