7314cc62665b1c12204ca6a7b1e5965e755b1852
navraj neupane <60267553+tesla-369@users.noreply.github.com>
2023-01-19 20:03:15 +0545

    Add files via upload

---
+++ whileloop2.java
@@ -0,0 +1,20 @@
+
+public class whileloop2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		  // initializing expression
+        int i = 1;
+ 
+        // test expression
+        while (i <= 6) {
+            System.out.println("Hello World");
+ 
+            // update expression
+            i++;
+        }
+    }
+}
+		
+	
