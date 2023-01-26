cb28a36f3958e6bd402222054ff9af8d679f09d3
navraj neupane <60267553+tesla-369@users.noreply.github.com>
2023-01-26 19:27:27 +0545

    Add files via upload

---
+++ JavaNotes/foreach_loop.java
@@ -0,0 +1,17 @@
+
+public class foreach_loop {
+	/*
+	  To access multiple value in a variable we use for loop
+	  syntax
+	  for(datatype var1:var2)
+	  {
+	  statement;
+	  }
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
