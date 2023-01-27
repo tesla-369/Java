7507b646fe91abfe65a461628f0925f7471eeaf2
navraj neupane <60267553+tesla-369@users.noreply.github.com>
2023-01-27 21:22:04 +0545

    Add files via upload

---
+++ JavaNotes/controlflow.java
@@ -0,0 +1,16 @@
+
+public class controlflow {
+	
+/*COntrol flow are transfer statements
+ * break //stop from body or take out from current iteration
+ * continue //skip
+ * return //returns value where we call method
+ * 
+ */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
