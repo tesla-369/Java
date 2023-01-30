5044e25a230064939ec6bd5b3e8b3b8202658e47
navraj neupane <60267553+tesla-369@users.noreply.github.com>
2023-01-30 20:57:17 +0545

    Add files via upload

---
+++ JavaNotes/switchstatement.java
@@ -0,0 +1,24 @@
+
+public class switchstatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*Switch is a multiple choice decision making selection statement.
+		 * It is used when we want to use only one case out of multiple cases.
+		 * 
+		 * Syntax
+		 * Switch(exp)
+		 * {
+		 * Case1:Statement 1;
+		 * break;
+		 * case2:Statement 2;
+		 * break;
+		 * casen:Statement n;
+		 * break;
+		 * default: Statement;
+		 * }
+		 */
+
+	}
+
+}
